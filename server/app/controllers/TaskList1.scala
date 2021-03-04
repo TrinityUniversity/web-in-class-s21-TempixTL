68f991c77bf770d732b31750f0e39ef50cd06cca
tempixtl <tom@lauerman.dev>
2021-03-03 23:35:50 -0600

    Finish making a model

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -29,8 +29,23 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
     }.getOrElse(Redirect(routes.TaskList1.login()))
   }
 
+  def createUser = Action { request =>
+    val postVals = request.body.asFormUrlEncoded
+    postVals.map { args => 
+      val username = args("username").head
+      val password = args("password").head
+
+      if (TaskListInMemoryModel.createUser(username, password)) {
+        Redirect(routes.TaskList1.taskList())
+      } else {
+        Redirect(routes.TaskList1.login())
+      }
+    }.getOrElse(Redirect(routes.TaskList1.login()))
+  }
+
   def taskList = Action {
-    val tasks = List("task1", "task2", "task3")
+    val username = "tommylau"
+    val tasks = TaskListInMemoryModel.getTasks(username)
     Ok(views.html.taskList1(tasks))
   }
 }
--- server/app/models/TaskListInMemoryModel.scala
+++ server/app/models/TaskListInMemoryModel.scala
@@ -4,14 +4,22 @@ import collection.mutable
 
 object TaskListInMemoryModel {
   private val users = mutable.Map[String, String]("tommylau" -> "mypass")
+  private val tasks = mutable.Map[String, List[String]]("tommylau" -> List("code", "rollerblade", "sleep"))
 
   def validateUser(username: String, password: String): Boolean = {
     users.get(username).map(_ == password).getOrElse(false)
   }
 
-  def createUser(username: String, password: String): Boolean = ???
+  def createUser(username: String, password: String): Boolean = {
+    if (users.contains(username)) false else {
+      users(username) = password
+      true
+    }
+  }
 
-  def getTasks(username: String): Seq[String] = ???
+  def getTasks(username: String): Seq[String] = {
+    tasks.get(username).getOrElse(Nil)
+  }
 
   def addTask(username: String, task: String): Unit = ???
 
