b646615f8b37006a61083b560e4b7c50c99e6883
tempixtl <tom@lauerman.dev>
2021-03-08 17:42:04 -0600

    [BROKEN] Exploring Play forms

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -4,9 +4,18 @@ import javax.inject._
 import play.api.mvc._
 import play.api.i18n._
 import models.TaskListInMemoryModel
+import play.api.data._
+import play.api.data.Forms._
+
+case class LoginData(username: String, password: String)
 
 @Singleton
 class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  val loginForm = Form(mapping(
+    "Username" -> text(3, 10),
+    "Password" -> text(8)
+  )(LoginData.apply)(LoginData.unapply))
+
   def login = Action { implicit request =>
     Ok(views.html.login1())
   }
@@ -27,6 +36,10 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
         Redirect(routes.TaskList1.login()).flashing("error" -> "Invalid username/passsword combination.")
       }
     }.getOrElse(Redirect(routes.TaskList1.login()))
+  }
+
+  def validateLoginForm = Action { implicit request =>
+    Ok("")
   }
 
   def createUser = Action { implicit request =>
