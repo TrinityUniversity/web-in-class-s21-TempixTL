bf950a310cdab109bd7c6916c655069491395a36
tempixtl <tom@lauerman.dev>
2021-03-10 18:33:48 -0600

    Write Task List tests (pt 3)

---
+++ server/test/ControllerSpec.scala
@@ -0,0 +1,19 @@
+import org.scalatestplus.play._
+import controllers.Application
+import play.api.test.Helpers
+import play.api.test.Helpers._
+import play.api.test.FakeRequest
+
+class ControllerSpec extends PlaySpec {
+  "Application#index" must {
+    "give back expected page" in {
+      val controller = new Application(Helpers.stubControllerComponents())
+      val result = controller.index.apply(FakeRequest())
+      val bodyText = contentAsString(result)
+
+      bodyText must include ("Play and Scala.js")
+      bodyText must include ("Play shouts out:")
+      bodyText must include ("Scala.js shouts out:")
+    }
+  }
+}
