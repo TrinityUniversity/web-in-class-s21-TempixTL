b2e060b3e667a8d02ed5479033d9ce93322567b2
tempixtl <tom@lauerman.dev>
2021-03-10 18:22:31 -0600

    Write Task List tests (pt 2)

--- server/test/TaskListInMemoryModelSpec.scala
+++ server/test/TaskListInMemoryModelSpec.scala
@@ -13,11 +13,39 @@ class TaskListInMemoryModelSpec extends PlaySpec {
     }
 
     "reject login with wrong username" in {
-      TaskListInMemoryModel.validateUser("Mark", "mypass") mustBe false
+      TaskListInMemoryModel.validateUser("wronguser", "mypass") mustBe false
+    }
+
+    "reject login with wrong username and password" in {
+      TaskListInMemoryModel.validateUser("wronguser", "wrongpass") mustBe false
     }
 
     "get correct default tasks" in {
       TaskListInMemoryModel.getTasks("tommylau") mustBe List("code", "rollerblade", "sleep")
+    }
+
+    "create new user with no tasks" in {
+      TaskListInMemoryModel.createUser("mark", "password") mustBe true
+      TaskListInMemoryModel.getTasks("mark") mustBe Nil
+    }
+
+    "create new user with existing name" in {
+      TaskListInMemoryModel.createUser("tommylau", "diffpass") mustBe false
+    }
+
+    "add new task for default user" in {
+      TaskListInMemoryModel.addTask("tommylau", "testing")
+      TaskListInMemoryModel.getTasks("tommylau") must contain ("testing")
+    }
+
+    "add new task for new user" in {
+      TaskListInMemoryModel.addTask("mark", "testing1")
+      TaskListInMemoryModel.getTasks("mark") must contain ("testing1")
+    }
+
+    "remove task from default user" in {
+      TaskListInMemoryModel.removeTask("tommylau", TaskListInMemoryModel.getTasks("tommylau").indexOf("code"))
+      TaskListInMemoryModel.getTasks("tommylau") must not contain ("code")
     }
   }
 }
