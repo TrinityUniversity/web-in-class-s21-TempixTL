0591db21b69a5dec39956dbc74a1b03de1338956
Thomas Lauerman <tom@lauerman.dev>
2021-03-14 16:49:05 -0500

    Convert createUser, addTask, deleteTask to POST

--- server/app/controllers/TaskList2.scala
+++ server/app/controllers/TaskList2.scala
@@ -17,40 +17,59 @@ class TaskList2 @Inject()(cc: ControllerComponents) extends AbstractController(c
     Ok(views.html.login2())
   }
 
-  def validate() = Action { implicit request =>
+  def validate = Action { implicit request =>
     val postVals = request.body.asFormUrlEncoded
     postVals.map { args => 
       val username = args("username").head
       val password = args("password").head
       if (TaskListInMemoryModel.validateUser(username, password)) {
-        Ok(views.html.taskList2(TaskListInMemoryModel.getTasks(username))).withSession("username" -> username)
+        Ok(views.html.taskList2(TaskListInMemoryModel.getTasks(username)))
+          .withSession(
+            "username" -> username,
+            "csrfToken" -> play.filters.csrf.CSRF.getToken.get.value)
       } else {
         Ok(views.html.login2())
       }
     }.getOrElse(Ok(views.html.login2()))
   }
 
-  def createUser(username: String, password: String) = Action {
-    if (TaskListInMemoryModel.createUser(username, password)) {
-      Ok(views.html.taskList2(TaskListInMemoryModel.getTasks(username))).withSession("username" -> username)
-    } else {
-      Ok(views.html.login2())
-    }
-  }
-
-  def deleteTask(index: Int) = Action { implicit request =>
-    val usernameOpt = request.session.get("username")
-    usernameOpt.map { username =>
-      TaskListInMemoryModel.removeTask(username, index)
-      Ok(views.html.taskList2(TaskListInMemoryModel.getTasks(username)))
+  def createUser = Action { implicit request =>
+    val postVals = request.body.asFormUrlEncoded
+    postVals.map { args => 
+      val username = args("username").head
+      val password = args("password").head
+      if (TaskListInMemoryModel.createUser(username, password)) {
+        Ok(views.html.taskList2(TaskListInMemoryModel.getTasks(username)))
+          .withSession(
+            "username" -> username,
+            "csrfToken" -> play.filters.csrf.CSRF.getToken.get.value)
+      } else {
+        Ok(views.html.login2())
+      }
     }.getOrElse(Ok(views.html.login2()))
   }
 
-  def addTask(task: String) = Action { implicit request =>
+  def deleteTask = Action { implicit request =>
     val usernameOpt = request.session.get("username")
     usernameOpt.map { username =>
-      TaskListInMemoryModel.addTask(username, task)
-      Ok(views.html.taskList2(TaskListInMemoryModel.getTasks(username)))
+      val postVals = request.body.asFormUrlEncoded
+      postVals.map { args =>
+        val index = args("index").head.toInt
+        TaskListInMemoryModel.removeTask(username, index)
+        Ok(views.html.taskList2(TaskListInMemoryModel.getTasks(username)))
+      }.getOrElse(Ok(views.html.login2()))
+    }.getOrElse(Ok(views.html.login2()))
+  }
+
+  def addTask = Action { implicit request =>
+    val usernameOpt = request.session.get("username")
+    usernameOpt.map { username =>
+      val postVals = request.body.asFormUrlEncoded
+      postVals.map { args =>
+        val task = args("task").head
+        TaskListInMemoryModel.addTask(username, task)
+        Ok(views.html.taskList2(TaskListInMemoryModel.getTasks(username)))
+      }.getOrElse(Ok(views.html.login2()))
     }.getOrElse(Ok(views.html.login2()))
   }
 
