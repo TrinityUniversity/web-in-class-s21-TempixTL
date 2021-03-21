2e3b4e42716ef7ba4dfeb52dac41aa234f9fd03e
Thomas Lauerman <tom@lauerman.dev>
2021-03-21 18:59:06 -0500

    Version 3 setup

---
+++ server/app/controllers/TaskList3.scala
@@ -0,0 +1,18 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+import play.api.i18n._
+import models.TaskListInMemoryModel
+import play.api.data._
+import play.api.data.Forms._
+import play.api.libs.json.Json
+
+@Singleton
+class TaskList3 @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
+    def load = TODO
+
+    def data = Action {
+        Ok(Json.toJson(Seq("a", "b", "c")))
+    }
+}
