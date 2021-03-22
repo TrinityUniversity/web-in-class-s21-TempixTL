0ec624d2562911c1badf3a5e69b1e9555cbd774b
tempixtl <tom@lauerman.dev>
2021-03-22 17:26:26 -0500

    Setting up for WebSockets

---
+++ server/public/javascript/chat.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/**
+ * Code for the WebSocket chat application.
+ */
+
+const inputField = document.getElementById('chat-input');
+const outputArea = document.getElementById('chat-area');
