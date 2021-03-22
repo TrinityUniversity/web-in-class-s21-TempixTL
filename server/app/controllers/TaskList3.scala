0c14b000b597379c507810efd112bbf2b51e300e
Thomas Lauerman <tom@lauerman.dev>
2021-03-21 19:27:09 -0500

    Implement most of validate

--- server/app/controllers/TaskList3.scala
+++ server/app/controllers/TaskList3.scala
@@ -7,14 +7,37 @@ import models.TaskListInMemoryModel
 import play.api.data._
 import play.api.data.Forms._
 import play.api.libs.json.Json
+import models.UserData
+import play.api.libs.json._
 
 @Singleton
 class TaskList3 @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
-    def load = Action { implicit request =>
-        Ok(views.html.version3Main())
-    }
+  implicit val userDataReads = Json.reads[UserData]
 
-    def data = Action {
-        Ok(Json.toJson(Seq("a", "b", "c")))
-    }
+  def load = Action { implicit request =>
+    Ok(views.html.version3Main())
+  }
+    
+  def data = Action {
+    Ok(Json.toJson(Seq("a", "b", "c")))
+  }
+    
+  def validate = Action { implicit request =>
+    request.body.asJson.map { body =>
+      Json.fromJson[UserData](body) match {
+        case JsSuccess(ud, path) => 
+          val username = ud.username
+          val password = ud.password
+          if (TaskListInMemoryModel.validateUser(username, password)) {
+            Ok(Json.toJson(true))
+              .withSession(
+                "username" -> username,
+                "csrfToken" -> play.filters.csrf.CSRF.getToken.get.value)
+          } else {
+            Ok(views.html.login2())
+          }
+        case e @ JsError(_) => Redirect(routes.TaskList3.load())
+      }
+    }.getOrElse(Redirect(routes.TaskList3.load()))
+  }
 }
