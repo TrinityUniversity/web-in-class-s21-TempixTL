fd35db48b53b21e0b9b191a89dc3140bfe103b2d
tempixtl <tom@lauerman.dev>
2021-03-28 11:48:35 -0500

    Complete brief overview of React (sans-JSX)

--- server/public/javascript/version4.js
+++ server/public/javascript/version4.js
@@ -12,15 +12,35 @@ class Hello extends React.Component {
     this.state = { clickCount: 0 };
   }
 
+  clickHandler(e) {
+    this.setState({clickCount: this.state.clickCount + 1});
+  }
+
   render() {
-    return ce('div', null, `Hello ${this.props.toWhat} - click count ${this.state.clickCount}`);
+    return ce('div', {onClick: (e) => this.clickHandler(e)}, `Hello ${this.props.toWhat} - click count ${this.state.clickCount}`);
+  }
+}
+
+class SimpleForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { textInput: '' };
+  }
+
+  changeHandler(event) {
+    this.setState({textInput: event.target.value});
+  }
+
+  render() {
+    return ce('input', {type: 'text', value: this.state.textInput, onChange: (e) => this.changeHandler(e)});
   }
 }
 
 ReactDOM.render(
   ce('div', null,
     ce(Hello, {toWhat: 'World'}, null),
-    ce(StatelessHello, {toWhat: 'World'}, null)
+    ce(StatelessHello, {toWhat: 'World'}, null),
+    ce(SimpleForm, null, null),
   ),
   document.getElementById('react-root')
 );
